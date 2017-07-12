415d624c14a693b1e100905f9e061f639034e79d
TatarchukKO <dolbyone007@gmail.com>
2017-07-12 19:17:57 +0300

    meta-data.js updated

--- server/models/meta-data.js
+++ server/models/meta-data.js
@@ -1,22 +1,35 @@
 const mysql = require('mysql');
-const metaDataQuery = require('./meta-data-queries.js');
+const metaDataQueries = require('../queries/meta-data-queries.js');
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'qweasdzxc',
+  password: '123789',
   database: 'pick_brains_db',
 });
 
 connection.connect((error) => {
   if (error) {
-    console.log('No connection!');
-    throw error;
+    console.log('Models connection error');
   }
-  console.log('Db connected');
+  console.log('Meta-data model is connected');
 });
 
-
 exports.getEnglishLevels = (callback) => {
-  connection.query(metaDataQuery.englishLevelsQuery, callback);
+  connection.query(metaDataQueries.englishLevelsQuery, callback);
+};
+exports.getLocations = (callback) => {
+  connection.query(metaDataQueries.locationQuery, callback);
+};
+exports.getSkills = (callback) => {
+  connection.query(metaDataQueries.skillsQuery, callback);
+};
+exports.getCandidateStatuses = (callback) => {
+  connection.query(metaDataQueries.candidateStatusesQuery, callback);
+};
+exports.getOtherSkills = (callback) => {
+  connection.query(metaDataQueries.otherSkillsQuery, callback);
+};
+exports.getVacancyStatuses = (callback) => {
+  connection.query(metaDataQueries.otherSkillsQuery, callback);
 };
