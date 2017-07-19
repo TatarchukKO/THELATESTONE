c16ca4015ab55879effc7607bb9cf6d374945ed6
Kanstantsin <dolbyone007@gmail.com>
2017-07-19 17:28:24 +0300

    small fixes

--- server/dao/meta-data.js
+++ server/dao/meta-data.js
@@ -2,22 +2,22 @@ const connection = require('./connection.js').connection;
 const metaDataQueries = require('../queries/meta-data-queries.js');
 
 function getEnglishLevels(callback) {
-  connection.query(metaDataQueries.englishLevelsQuery, callback);
+  connection.query(metaDataQueries.englishLevelsQuery(), callback);
 }
 function getLocations(callback) {
-  connection.query(metaDataQueries.locationQuery, callback);
+  connection.query(metaDataQueries.locationQuery(), callback);
 }
 function getSkills(callback) {
-  connection.query(metaDataQueries.skillsQuery, callback);
+  connection.query(metaDataQueries.skillsQuery(), callback);
 }
 function getCandidateStatuses(callback) {
-  connection.query(metaDataQueries.candidateStatusesQuery, callback);
+  connection.query(metaDataQueries.candidateStatusesQuery(), callback);
 }
 function getOtherSkills(callback) {
-  connection.query(metaDataQueries.otherSkillsQuery, callback);
+  connection.query(metaDataQueries.otherSkillsQuery(), callback);
 }
 function getVacancyStatuses(callback) {
-  connection.query(metaDataQueries.otherSkillsQuery, callback);
+  connection.query(metaDataQueries.otherSkillsQuery(), callback);
 }
 
 module.exports = {
