36359c51eb895c181f84eee970d988da8234d03f
Kapysta0605 <shavel.ilya@gmail.com>
2017-07-19 15:18:29 +0300

    shiet

--- server/queries/meta-data-queries.js
+++ server/queries/meta-data-queries.js
@@ -1,6 +1,6 @@
-exports.englishLevelsQuery = 'SELECT english_lvl.lvl FROM english_lvl';
-exports.locationQuery = 'SELECT location.city FROM location';
-exports.skillsQuery = 'SELECT skills.skill_name FROM skills';
+exports.englishLevelsQuery = 'SELECT * FROM english_lvl';
+exports.locationQuery = 'SELECT * FROM location';
+exports.skillsQuery = 'SELECT * FROM skills';
 exports.candidateStatusesQuery = 'SELECT * FROM candidate_status';
 exports.otherSkillsQuery = 'SELECT * FROM other_skills';
 exports.vacancyStatusesQuery = 'SELECT * FROM vacancy_status';
