7fcdca27daa843838734f57f2f553a1078c40669
Kanstantsin <dolbyone007@gmail.com>
2017-07-27 14:13:06 +0300

    LIMIT 20 from skills removed

--- server/queries/meta-data-queries.js
+++ server/queries/meta-data-queries.js
@@ -1,6 +1,6 @@
 function englishLevelsQuery() { return 'SELECT * FROM english_lvl'; }
 function locationQuery() { return 'SELECT * FROM location'; }
-function skillsQuery() { return 'SELECT * FROM skills LIMIT 20'; }
+function skillsQuery() { return 'SELECT * FROM skills'; }                        /* LIMIT 20*/
 function candidateStatusesQuery() { return 'SELECT * FROM candidate_status'; }
 function otherSkillsQuery() { return 'SELECT * FROM other_skills'; }
 function vacancyStatusesQuery() { return 'SELECT * FROM vacancy_status'; }
