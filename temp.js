ab109ba8740066a12a32c112a92317258d96ed05
Kapysta0605 <shavel.ilya@gmail.com>
2017-08-02 19:28:12 +0300

    removed empty files

--- temp.js
+++
@@ -1,9 +0,0 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-
-
-xhr.open('POST', 'http://localhost:1337/api/candidates/report', false);
-
-xhr.send();
-const fs = require('fs');
-fs.writeFileSync('dataaaaaaa.xlsx', xhr.responseText, 'binary');
