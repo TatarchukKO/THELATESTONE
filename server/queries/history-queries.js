72af82a7a26cfde4baf9fa156c1feac0bc7308c3
BeAndy <belous.andrew77@gmail.com>
2017-08-04 14:14:32 +0300

    General history fix

--- server/queries/history-queries.js
+++ server/queries/history-queries.js
@@ -32,25 +32,27 @@ const getHistory = () =>
     SELECT
       vacancy_change_id, candidate_change_id, hrm_feedback_id,
       ts_feedback_id, general_history.interview_id, general_history.change_date,
-      hrm_feedback.user_id, users.first_name, users.second_name,
-      hrm_feedback.vacancy_id, vacancy.name
+      interview.user_id, users.first_name, users.second_name,
+      interview.vacancy_id, vacancy.name
       FROM general_history
       INNER JOIN hrm_feedback ON hrm_feedback_id = hrm_feedback.id 
+      LEFT JOIN interview ON hrm_feedback.interview_id = interview.id
       LEFT JOIN users ON users.id = user_id
       LEFT JOIN vacancy ON vacancy.id = vacancy_id
    UNION
      SELECT
       vacancy_change_id, candidate_change_id, hrm_feedback_id,
       ts_feedback_id, general_history.interview_id, general_history.change_date,
-      ts_feedback.user_id, users.first_name, users.second_name,
-      ts_feedback.vacancy_id, vacancy.name
+      interview.user_id, users.first_name, users.second_name,
+      ts_feedback.interview_id, vacancy.name
       FROM general_history
       INNER JOIN ts_feedback ON ts_feedback_id = ts_feedback.id
+      LEFT JOIN interview ON ts_feedback.interview_id = interview.id
       LEFT JOIN users ON users.id = user_id
       LEFT JOIN vacancy ON vacancy.id = vacancy_id`;
 
 const getCandidatesNames = () =>
-  `SELECT candidate_id, candidate.ru_first_name, candidate.ru_second_name,
+  `SELECT candidate_changes.id, candidate.ru_first_name, candidate.ru_second_name,
   candidate.eng_first_name, candidate.eng_second_name
   FROM candidate_changes
   LEFT JOIN candidate ON candidate.id = candidate_id`;
